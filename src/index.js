c0e4f9932a28596b2aecfd0c007c9f3641da7018
Phillipa2 <phillipaatieno7@gmail.com>
2026-05-15 17:10:09 -0700

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ function generatePoem(event) {
     let prompt = `User Instructions: Generate a Personalized ${instructionsInput.value}`;
     let context = "You are a creative poet who writes deeply personal and emotionally touching birthday poems based on the details and memories provided. Your mission is to generate a 6 line poem in basic HTML. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<span class="blink">⏳ Generating a birthday poem about ${instructionsInput.value}..</span>`;
     
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
